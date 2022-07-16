735a2544db10b5fa368ed698969ade0524023962
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-07-16 19:31:42 +0300

    add numbered compiling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from text_segment import *
-from pass_and_compile import *
+from text_segment import text_segment
+from pass_and_compile import numbered_pass_and_compile
 
 
 if __name__== "__main__":
 #	print(text_segment("./test"))
-#	pass_and_compile("./test", "./compile", "*.c", "g++", ["-std=gnu++11"])
+#	numbered_pass_and_compile("./test", "./compile", "*.c", "g++", ["-std=gnu++11"])
--- pass_and_compile.py
+++ pass_and_compile.py
@@ -3,11 +3,43 @@ import fnmatch
 import random
 
 
+# Number for naming compiled files. It is incrementing while files are being compiled.
+# Default value is 1
+numbered_name = 1
+
+
 # -----------------------------------------------------------
 # Passes through given source directory and it's subdirectories,
 # compiles with given compiler and flags all finded files with given pattern
 # and puts binaries in given directory for compiled files. Compiled files have
-# random names.
+# numbered names. It sets number for naming before and after calling.
+#
+# Parameters:
+#
+# source: path to directory with source files for compiling
+# compiled: path to directory for putting compiled files
+# pattern: regular expression for name of source files. For example, "*.c"
+# compiler: compiler for compiling
+# flags: list of flags for compiling
+# -----------------------------------------------------------
+def numbered_pass_and_compile(source, compiled, pattern, compiler, flags):
+	global numbered_name
+
+	# Setting default value for number for naming
+	numbered_name = 1
+	# Passing and compiling
+	pass_and_compile(source, compiled, pattern, compiler, flags)
+	# Setting default value for number for naming
+	numbered_name = 1
+
+
+# -----------------------------------------------------------
+# DON'T USE THIS FUNCTION OUTSIDE FILE
+
+# Passes through given source directory and it's subdirectories,
+# compiles with given compiler and flags all finded files with given pattern
+# and puts binaries in given directory for compiled files. Compiled files have
+# numbered names.
 #
 # Parameters:
 #
@@ -19,6 +51,8 @@ import random
 # -----------------------------------------------------------
 # FIXME try to divide it in to functions: pass with function parameter and compile
 def pass_and_compile(source, compiled, pattern, compiler, flags):
+	global numbered_name
+
 	# Passing through all subdirectories
 	for subdir in os.listdir(source):
 		# Getting subdirectory's path
@@ -37,10 +71,12 @@ def pass_and_compile(source, compiled, pattern, compiler, flags):
 			# Setting path to source file
 			compiling_command = compiling_command + path_to_subdir + " -o "
 			# Setting path for binary file
-			compiling_command = compiling_command + compiled + "/" + str(random.randint(0, 1000000000)) + " "
+			compiling_command = compiling_command + compiled + "/" + str(numbered_name) + " "
 			# Removing output
 			compiling_command = compiling_command + "2> /dev/null"
 			# Executing command
 			os.system(compiling_command)
+			# Incrementing number for naming
+			numbered_name = numbered_name + 1
 
 
