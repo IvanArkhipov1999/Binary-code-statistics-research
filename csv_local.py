a904a12213efc956c7117d227b22f5650343252c
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-07-18 10:52:37 +0300

    a add_data_to_csv function

--- csv_local.py
+++ csv_local.py
@@ -1,17 +1,32 @@
 import csv
+import pandas
 
 
 # -----------------------------------------------------------
-# FIXME: NOW THIS FUNCTION REWRITES CSV FILE, NOT ADDS
-# Writes data to the given csv file.
+# Writes data to the given csv file. If there was data in csv,
+# it rewrites data.
+# Parameters:
+#
+# path_to_file: path to csv file
+# data: numpy array
+# -----------------------------------------------------------
+def write_data_to_csv(path_to_file, data):
+	with open(path_to_file, mode="w", encoding='utf-8') as csv_file:
+		writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+		for row in data:
+			writer.writerow(row)
+
+
+# -----------------------------------------------------------
+# Writes data to the given csv file. Add new columns in csv file.
 # Parameters:
 #
 # path_to_file: path to csv file
 # data: numpy array
 # -----------------------------------------------------------
 def add_data_to_csv(path_to_file, data):
-	with open(path_to_file, mode="w", encoding='utf-8') as csv_file:
-		writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-		for row in data:
-			writer.writerow(row)
+	dataframe = pandas.read_csv(path_to_file)
+	for column in data.transpose():
+		dataframe[column[0]] = column[1:]
+	dataframe.to_csv(path_to_file, index=False)
 
