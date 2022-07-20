35121a2374be4eddf9e44c988c9cadb04419e3b2
IvanArkhipov1999 <arkhipov.iv99@mail.ru>
2022-07-20 10:58:31 +0300

    add proportions_length_4

--- statistics.py
+++ statistics.py
@@ -248,3 +248,181 @@ def proportions_length_3(source):
 		propotions_of_101, propotions_of_110, propotions_of_111]).transpose()
 
 
+# -----------------------------------------------------------
+# Passes through given source directory and it's subdirectories,
+# for each file counts propotion of each binary substring with length 4.
+# Returns numpy array with propotions. Each column of numpy array contains
+# propotions of certain substring for all files. Substrings arranged 
+# in lexicographic order.
+#
+# Parameters:
+#
+# source: path to directory with executable binary files
+# -----------------------------------------------------------
+def proportions_length_4(source):
+	# List with propotions of "0000" substring
+	propotions_of_0000 = ["0000--x86_64-g++-ubuntu"]
+	# List with propotions of "0001" substring
+	propotions_of_0001 = ["0001--x86_64-g++-ubuntu"]
+	# List with propotions of "0010" substring
+	propotions_of_0010 = ["0010--x86_64-g++-ubuntu"]
+	# List with propotions of "0011" substring
+	propotions_of_0011 = ["0011--x86_64-g++-ubuntu"]
+	# List with propotions of "0100" substring
+	propotions_of_0100 = ["0100--x86_64-g++-ubuntu"]
+	# List with propotions of "0101" substring
+	propotions_of_0101 = ["0101--x86_64-g++-ubuntu"]
+	# List with propotions of "0110" substring
+	propotions_of_0110 = ["0110--x86_64-g++-ubuntu"]
+	# List with propotions of "0111" substring
+	propotions_of_0111 = ["0111--x86_64-g++-ubuntu"]
+	# List with propotions of "1000" substring
+	propotions_of_1000 = ["1000--x86_64-g++-ubuntu"]
+	# List with propotions of "1001" substring
+	propotions_of_1001 = ["1001--x86_64-g++-ubuntu"]
+	# List with propotions of "1010" substring
+	propotions_of_1010 = ["1010--x86_64-g++-ubuntu"]
+	# List with propotions of "1011" substring
+	propotions_of_1011 = ["1011--x86_64-g++-ubuntu"]
+	# List with propotions of "1100" substring
+	propotions_of_1100 = ["1100--x86_64-g++-ubuntu"]
+	# List with propotions of "1101" substring
+	propotions_of_1101 = ["1101--x86_64-g++-ubuntu"]
+	# List with propotions of "1110" substring
+	propotions_of_1110 = ["1110--x86_64-g++-ubuntu"]
+	# List with propotions of "1111" substring
+	propotions_of_1111 = ["1111--x86_64-g++-ubuntu"]
+
+	# Passing and counting propotions function
+	def pass_and_count(source):
+		nonlocal propotions_of_0000, propotions_of_0001, propotions_of_0010, propotions_of_0011, \
+ 			propotions_of_0100, propotions_of_0101, propotions_of_0110, propotions_of_0111, \
+			propotions_of_1000, propotions_of_1001, propotions_of_1010, propotions_of_1011, \
+			propotions_of_1100, propotions_of_1101, propotions_of_1110, propotions_of_1111
+
+		# Passing through all subdirectories
+		for subdir in os.listdir(source):
+			# Getting subdirectory's path
+			path_to_subdir = source + "/" + subdir
+
+			# If it is subdirectory then call this function
+			if os.path.isdir(path_to_subdir):
+				pass_and_count(path_to_subdir)
+			# If it is file
+			else:
+				# Printing file name
+				print(path_to_subdir)
+				# Getting text segment in bytes
+				bytes = text_segment(path_to_subdir)
+				# Converting text segment to bits
+				bits = numpy.unpackbits(bytes)
+
+				# Counter for "0000" substrings in file
+				counter_0000 = 0
+				# Counter for "0001" substrings in file
+				counter_0001 = 0
+				# Counter for "0010" substrings in file
+				counter_0010 = 0
+				# Counter for "0011" substrings in file
+				counter_0011 = 0
+				# Counter for "0100" substrings in file
+				counter_0100 = 0
+				# Counter for "0101" substrings in file
+				counter_0101 = 0
+				# Counter for "0110" substrings in file
+				counter_0110 = 0
+				# Counter for "0111" substrings in file
+				counter_0111 = 0
+				# Counter for "1000" substrings in file
+				counter_1000 = 0
+				# Counter for "1001" substrings in file
+				counter_1001 = 0
+				# Counter for "1010" substrings in file
+				counter_1010 = 0
+				# Counter for "1011" substrings in file
+				counter_1011 = 0
+				# Counter for "1100" substrings in file
+				counter_1100 = 0
+				# Counter for "1101" substrings in file
+				counter_1101 = 0
+				# Counter for "1110" substrings in file
+				counter_1110 = 0
+				# Counter for "1111" substrings in file
+				counter_1111 = 0
+
+				# Cycle throuth bits for counting substrings
+				for i in range(0, len(bits) - 3):
+					if bits[i] == 0 and bits[i + 1] == 0 and bits[i + 2] == 0 and bits[i + 3] == 0:
+						counter_0000 = counter_0000 + 1
+					elif bits[i] == 0 and bits[i + 1] == 0 and bits[i + 2] == 0 and bits[i + 3] == 1:
+						counter_0001 = counter_0001 + 1
+					elif bits[i] == 0 and bits[i + 1] == 0 and bits[i + 2] == 1 and bits[i + 3] == 0:
+						counter_0010 = counter_0010 + 1
+					elif bits[i] == 0 and bits[i + 1] == 0 and bits[i + 2] == 1 and bits[i + 3] == 1:
+						counter_0011 = counter_0011 + 1
+					elif bits[i] == 0 and bits[i + 1] == 1 and bits[i + 2] == 0 and bits[i + 3] == 0:
+						counter_0100 = counter_0100 + 1
+					elif bits[i] == 0 and bits[i + 1] == 1 and bits[i + 2] == 0 and bits[i + 3] == 1:
+						counter_0101 = counter_0101 + 1
+					elif bits[i] == 0 and bits[i + 1] == 1 and bits[i + 2] == 1 and bits[i + 3] == 0:
+						counter_0110 = counter_0110 + 1
+					elif bits[i] == 0 and bits[i + 1] == 1 and bits[i + 2] == 1 and bits[i + 3] == 1:
+						counter_0111 = counter_0111 + 1
+					elif bits[i] == 1 and bits[i + 1] == 0 and bits[i + 2] == 0 and bits[i + 3] == 0:
+						counter_1000 = counter_1000 + 1
+					elif bits[i] == 1 and bits[i + 1] == 0 and bits[i + 2] == 0 and bits[i + 3] == 1:
+						counter_1001 = counter_1001 + 1
+					elif bits[i] == 1 and bits[i + 1] == 0 and bits[i + 2] == 1 and bits[i + 3] == 0:
+						counter_1010 = counter_1010 + 1
+					elif bits[i] == 1 and bits[i + 1] == 0 and bits[i + 2] == 1 and bits[i + 3] == 1:
+						counter_1011 = counter_1011 + 1
+					elif bits[i] == 1 and bits[i + 1] == 1 and bits[i + 2] == 0 and bits[i + 3] == 0:
+						counter_1100 = counter_1100 + 1
+					elif bits[i] == 1 and bits[i + 1] == 1 and bits[i + 2] == 0 and bits[i + 3] == 1:
+						counter_1101 = counter_1101 + 1
+					elif bits[i] == 1 and bits[i + 1] == 1 and bits[i + 2] == 1 and bits[i + 3] == 0:
+						counter_1110 = counter_1110 + 1
+					elif bits[i] == 1 and bits[i + 1] == 1 and bits[i + 2] == 1 and bits[i + 3] == 1:
+						counter_1111 = counter_1111 + 1
+
+				# Counting propotion of "0000" substring
+				propotions_of_0000.append(counter_0000 / (len(bits) - 3))
+				# Counting propotion of "0001" substring
+				propotions_of_0001.append(counter_0001 / (len(bits) - 3))
+				# Counting propotion of "0010" substring
+				propotions_of_0010.append(counter_0010 / (len(bits) - 3))
+				# Counting propotion of "0011" substring
+				propotions_of_0011.append(counter_0011 / (len(bits) - 3))
+				# Counting propotion of "0100" substring
+				propotions_of_0100.append(counter_0100 / (len(bits) - 3))
+				# Counting propotion of "0101" substring
+				propotions_of_0101.append(counter_0101 / (len(bits) - 3))
+				# Counting propotion of "0110" substring
+				propotions_of_0110.append(counter_0110 / (len(bits) - 3))
+				# Counting propotion of "0111" substring
+				propotions_of_0111.append(counter_0111 / (len(bits) - 3))
+				# Counting propotion of "1000" substring
+				propotions_of_1000.append(counter_1000 / (len(bits) - 3))
+				# Counting propotion of "1001" substring
+				propotions_of_1001.append(counter_1001 / (len(bits) - 3))
+				# Counting propotion of "1010" substring
+				propotions_of_1010.append(counter_1010 / (len(bits) - 3))
+				# Counting propotion of "1011" substring
+				propotions_of_1011.append(counter_1011 / (len(bits) - 3))
+				# Counting propotion of "1100" substring
+				propotions_of_1100.append(counter_1100 / (len(bits) - 3))
+				# Counting propotion of "1101" substring
+				propotions_of_1101.append(counter_1101 / (len(bits) - 3))
+				# Counting propotion of "1110" substring
+				propotions_of_1110.append(counter_1110 / (len(bits) - 3))
+				# Counting propotion of "1111" substring
+				propotions_of_1111.append(counter_1111 / (len(bits) - 3))
+	
+	# Passing and counting propotions
+	pass_and_count(source)
+
+	# Converting propotions to required form
+	return numpy.asarray([propotions_of_0000, propotions_of_0001, propotions_of_0010, propotions_of_0011, \
+ 			propotions_of_0100, propotions_of_0101, propotions_of_0110, propotions_of_0111, \
+			propotions_of_1000, propotions_of_1001, propotions_of_1010, propotions_of_1011, \
+			propotions_of_1100, propotions_of_1101, propotions_of_1110, propotions_of_1111]).transpose()
