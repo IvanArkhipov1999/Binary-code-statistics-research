58f382030680472dfef51f167857bc434059a65b
Ivan Arkhipov <arkhipov.iv99@mail.ru>
2023-03-20 17:21:55 +0300

    for merging with local rep

--- main.py
+++ main.py
@@ -6,15 +6,34 @@ from r_code_generator import *
 
 
 if __name__== "__main__":
+# Printing text_segment examples
 #	print(text_segment("../test86"))
 #	print(text_segment("../test"))
+#	print(text_segment("./compile/1"))
+
+
+# Passing and compiling examples
+#	numbered_pass_and_compile("./test", "./compile", "*.c", "mips64el-linux-gnuabi64-g++", ["-std=gnu++11", "-march=mips64"])
 #	numbered_pass_and_compile("./test", "./compile", "*.c", "g++", ["-std=gnu++11"])
+
 #	numbered_pass_and_compile("../gcc/libstdc++-v3/testsuite", "../Binaries-dataset/arm-g++-ubuntu", "*.cc", "arm-linux-gnueabihf-g++", ["-std=gnu++11", "-I../gcc/libstdc++-v3/testsuite/util"])
 #	numbered_pass_and_compile("../gcc/libstdc++-v3/testsuite", "../Binaries-dataset/arm64-g++-ubuntu", "*.cc", "aarch64-linux-gnu-g++", ["-std=gnu++11", "-I../gcc/libstdc++-v3/testsuite/util"])
 #	numbered_pass_and_compile("../GCC/libstdc++-v3/testsuite", "../Binaries-dataset/x86_64-g++-ubuntu", "*.cc", "g++", ["-std=gnu++11", "-I../GCC/libstdc++-v3/testsuite/util"])
+#	numbered_pass_and_compile("../gcc/libstdc++-v3/testsuite", "../Binaries-dataset/mips64el-g++-ubuntu", "*.cc", "mips64el-linux-gnuabi64-g++", ["-std=gnu++11", "-I../gcc/libstdc++-v3/testsuite/util", "-march=mips64"])
+
+
+# Preparing data examples
+#	write_data_to_csv("./data.csv", proportions_length("./compile", 1, "--arm64-g++-ubuntu"))
+#	for i in range(2, 11):
+#		add_data_to_csv("./data.csv", proportions_length("./compile", i, "--arm64-g++-ubuntu"))
+
+
+#	write_data_to_csv("./data_mips64el.csv", proportions_length("../Binaries-dataset/mips64el-g++-ubuntu", 1, "--mips64el-g++-ubuntu"))
+#	for i in range(2, 11):
+#		add_data_to_csv("./data_mips64el.csv", proportions_length("../Binaries-dataset/mips64el-g++-ubuntu", i, "--mips64el-g++-ubuntu"))
+
 #	write_data_to_csv("./data.csv", proportions_length_1("../Binaries-dataset/x86_64-g++-ubuntu"))
 #	write_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 1))
-#	add_data_to_csv("./data.csv", proportions_length_2("../Binaries-dataset/x86_64-g++-ubuntu"))
 #	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 2))
 #	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 3))
 #	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 4))
@@ -23,11 +42,11 @@ if __name__== "__main__":
 #	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 7))
 #	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 8))
 #	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 9))
-	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 10))
-#	add_data_to_csv("./data.csv", proportions_length_3("../Binaries-dataset/x86_64-g++-ubuntu"))
-#	add_data_to_csv("./data.csv", proportions_length_4("../Binaries-dataset/x86_64-g++-ubuntu"))
+#	add_data_to_csv("./data_arm64.csv", proportions_length("../Binaries-dataset/arm64-g++-ubuntu", 10))
+
+
 #	add_data_to_csv("./data.csv", proportions_length("../Binaries-dataset/x86_64-g++-ubuntu", 7))
 #	add_data_to_csv("./data.csv", proportions_length("../Binaries-dataset/x86_64-g++-ubuntu", 8))
 #	add_data_to_csv("./data.csv", proportions_length("../Binaries-dataset/x86_64-g++-ubuntu", 9))
+
 #	write_data_to_csv("./data1.csv", proportions_length("./compile", 9))
-    generate_code('code.txt', 10)
